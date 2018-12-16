2d408bae3cba5e0020202a4c67d697e351d37d18
David Banks <amoebae@gmail.com>
2018-12-16 20:03:41 +0000

    example usage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import WidgetView from 'amoe-butterworth-widgets';
 import mountWidgets from 'amoe-butterworth-widgets';
 
-import style from '../node_modules/amoe-butterworth-widgets/dist/amoe-butterworth-widgets.css';
+// load the css
+import 'amoe-butterworth-widgets/dist/amoe-butterworth-widgets.css';
 
 function onReady() {
     mountWidgets('#app');
