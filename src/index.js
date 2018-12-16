f681146863347245340fe392c28b5054d18ba4d7
David Banks <amoebae@gmail.com>
2018-12-16 19:46:28 +0000

    upgrading to explicit export version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import WidgetView from 'amoe-butterworth-widgets';
+import foo from 'amoe-butterworth-widgets';
 
 function onReady() {
-    console.log("widget view is %o", WidgetView);
+    console.log("exported value was %o", foo);
 }
 
 document.addEventListener('DOMContentLoaded', onReady);
