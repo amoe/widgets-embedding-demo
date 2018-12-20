426285599fe3f96acfc2daebc7883ba9c16400af
David Banks <amoebae@gmail.com>
2018-12-20 18:33:10 +0000

    button to generate widget output

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import mountWidgets from 'amoe-butterworth-widgets';
 // load the css
 import 'amoe-butterworth-widgets/dist/amoe-butterworth-widgets.css';
 
+function onClick() {
+    console.log("output button clicked");
+}
+
 function onReady() {
+    const outputButton = document.getElementById('output-button');
+    outputButton.addEventListener('click', onClick);
+
     mountWidgets('#app');
+
 }
 
 document.addEventListener('DOMContentLoaded', onReady);
